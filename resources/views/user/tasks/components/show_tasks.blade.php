f00e390cc4d294d12eb55aa45f2821fa0aaf4ffb
Agner Alexis Plascencia Amador <agner1803@hotmail.com>
2022-06-18 14:13:05 -0500

    feat(frontend): Implementacion de la vista de tareas del usuario

---
+++ resources/views/user/tasks/components/show_tasks.blade.php
@@ -0,0 +1,34 @@
+<div class="mt-4 p-3">
+    <h2>Tareas</h2>
+    @if (!$tasks->isEmpty())
+    <div class="table-responsive w-auto">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Acción</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <th>{{$task->name}}</th>
+                        <th>
+                            <form method="POST" action="{{route('user.tasks.destroy', ['task' => $task->id])}}">
+                                @csrf
+                                @method("delete")
+                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                            </form>
+                        </th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    @else
+        <div class="alert alert-danger d-flex align-items-center justify-content-center text-center py-2" role="alert">
+            {{-- <i class="material-icons icon text-danger">warning</i> --}}
+                    No hay tareas registradas
+        </div>
+    @endif
+</div>
---
+++ resources/views/user/tasks/tasks_index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container w-auto">
+    <div class="bg-white border border-dark px-3">
+        <h3 class="text-center border-bottom pt-2 pb-2">Agregar una tarea</h3>
+
+        {{-- Muestra mensaje de exito al crear la liga --}}
+        @include('components.show-messages-success-errors')
+
+        <div class="p-3 ">
+            <form method="POST" action="{{ isset($action)? $action : '' }}">
+                @csrf
+                @method(isset($method) ? $method : 'POST')
+                <div class="mb-3">
+                  <label
+                    for="task"
+                    class="form-label
+                        @error('task')
+                            is-invalid
+                        @enderror
+                    "
+                    @error('task')
+                        autofocus
+                    @enderror
+                    >Nombre:</label>
+                  <input type="text" name="task" class="form-control" id="task" aria-describedby="taskHelp">
+                  <div id="taskHelp" class="form-text">Nombre de la tarea.</div>
+                  @error('task')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+                </div>
+                @csrf
+                @method('POST')
+                <button type="submit" class="btn btn-primary">Agregar</button>
+              </form>
+        </div>
+    </div>
+
+    @include('user.tasks.components.show_tasks')
+</div>
+
+@endsection
+

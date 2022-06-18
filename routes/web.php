2a449c3a292848c7a281f9209b7c17f7d8ed33e8
Agner Alexis Plascencia Amador <agner1803@hotmail.com>
2022-06-18 14:09:51 -0500

    feat(backend): definicion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->prefix('user')->name('user.')->group(function () {
+    Route::resource('tasks', TaskController::class)
+        ->parameters(['task' => 'id'])
+        ->except([
+            'create'
+        ]);
+});
